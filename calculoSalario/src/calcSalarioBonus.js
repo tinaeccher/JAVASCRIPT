f649a49ab5bb5724559c64b4403933df644d3740
TinaEccher <cristinadeccher@gmail.com>
2026-05-17 07:06:13 -0300

    Exercício de JS  e mocha
    Exercício de Java Script Basico com teste em mocha, de calculo de adicional no salário

---
+++ calculoSalario/src/calcSalarioBonus.js
@@ -0,0 +1,24 @@
+/*
+Preciso de uma função capaz de receber o salário de um funcionário e sua senioridade atual (junior, pleno ou senior).
+A função terá que aplicar um bônus conforme a senioridade: junior = 50, pleno = 100 e senior = 1000.
+A função deve retornar o valor do salário já com bônus embutido.
+
+1. Entradas
++ Salário do funcionário;
++ Senioridade do funcionário;
+
+2. Regras de negócio
++ Salário deve ser um numero decimal positivo;
++ Senioridade deve ser uma string com os seguintes valores: "junior", "pleno" ou "senior";
+
+3. Processamento
++ Bonus junior = 50;
++ Bonus pleno = 100;
++ Bonus senior = 1000;
++ Salário final = salário + bônus;
+
+4. Saídas
+Retornar o valor do salário somado com o bonus, conforme a senioridade do funcionário.
+
+*/
+
