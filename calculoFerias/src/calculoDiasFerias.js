f70f59ed7adc919c1f1f8b776b936e9174098fd7
TinaEccher <cristinadeccher@gmail.com>
2026-05-24 20:59:07 -0300

    Exercício para calculo do valor a receber de um dia de férias
    Exercício para calculo do valor a receber de um dia de férias

---
+++ calculoFerias/src/calculoDiasFerias.js
@@ -0,0 +1,28 @@
+/*
+Preciso de uma função que seja capaz de permitir que o usuário venda dias de suas férias.
+Basicamente os dias de férias são calculados com base no valor bruto do salário mensal 
+(sempre contando com 30, independente da qtde de dias no mês). Logo, um funcionários que ganha
+3000 por mês e quer vender 1 dia de férias, vai receber 100 reais por esse dia (3000 / 30 = 100). 
+
+Entradas:
++ Valor do salário mensal
++ Quantidade de dias de férias vendidos
+
+Regras:
++ Salário é um valor decimal positivo
++ Quantidade de dias vendidos é um valor inteiro positivo entre 1 e 30
+
+Processamento:
+1. Calcular o valor do dia de férias (salário mensal / 30)
+2. Calcular o valor total recebido (valor do dia * quantidade de dias vendidos)
+
+Saídas:
++ Valor diário recebido por cada dia de férias vendido
++ Valor total recebido pelas férias vendidas
+*/
+
+export function calculoDiasFerias(salarioMensal, diasVendidos){
+    const valorPeloDiaFerias = salarioMensal / 30;
+        return valorPeloDiaFerias * diasVendidos;
+    }
+    console.log(calculoDiasFerias(3000, 1));
---
+++ calculoFerias/test/calculoDiasFerias.test.js
@@ -0,0 +1,17 @@
+import calculoDiasFerias from '../src/calculoDiasFerias.js';
+import assert from 'node:assert';
+
+describe('Testes para a função calculoDiasFerias', function() { 
+    it.only('Validar ao vender 1 dia de férias tendo o salário mensal de 3000, recebo 100 reais', function () {
+       //ARRANGE
+        const salarioMensal = 3000;
+        const diasVendidos = 1;
+   
+    //ACT
+        const valorAReceber = calculoDiasFerias(salarioMensal, diasVendidos);        
+
+    //ASSERT
+        assert.equal(valorAReceber, 100);
+    
+    });
+});
